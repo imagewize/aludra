b9a1ff37d9b6d23285575cadd67fcc38da427d3a
Jasper Frumau <jasper@imagewize.com>
2026-07-10 14:29:32 +0700

    Add faq-tab-answer block

---
+++ blocks/faq-tab-answer/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '27f26456a5548c19a369');
---
+++ blocks/faq-tab-answer/src/edit.js
@@ -0,0 +1,95 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import {
+	useBlockProps,
+	InnerBlocks,
+	RichText,
+	InspectorControls,
+} from '@wordpress/block-editor';
+
+import { PanelBody, TextControl } from '@wordpress/components';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @param {Object} props Block properties
+ * @return {Element} Element to render.
+ */
+export default function Edit({ attributes, setAttributes }) {
+	const { question, title } = attributes;
+
+	const blockProps = useBlockProps({
+		className: 'faq-tab-answer-editor',
+	});
+
+	return (
+		<>
+			<InspectorControls>
+				<PanelBody title={__('Tab Settings', 'aludra')}>
+					<TextControl
+						label={__('Question Text', 'aludra')}
+						value={question}
+						onChange={(value) => setAttributes({ question: value })}
+						help={__('The question shown in the tab navigation', 'aludra')}
+					/>
+				</PanelBody>
+			</InspectorControls>
+
+			<div {...blockProps}>
+				<div className="faq-answer-header">
+					<RichText
+						tagName="h3"
+						className="faq-answer-title"
+						value={title}
+						onChange={(value) => setAttributes({ title: value })}
+						placeholder={__('Enter answer title...', 'aludra')}
+					/>
+				</div>
+				<div className="faq-answer-content">
+					<InnerBlocks
+						template={[
+							[
+								'core/paragraph',
+								{
+									content: __(
+										'We provide a comprehensive range of professional services tailored to meet your specific needs. Our experienced team specializes in delivering high-quality solutions that drive results and exceed expectations.',
+										'aludra'
+									),
+								},
+							],
+							[
+								'core/paragraph',
+								{
+									content: __(
+										'Whether you\'re looking for strategic consulting, creative design, technical development, or ongoing support, we have the expertise and resources to help you succeed.',
+										'aludra'
+									),
+								},
+							],
+						]}
+					/>
+				</div>
+			</div>
+		</>
+	);
+}
