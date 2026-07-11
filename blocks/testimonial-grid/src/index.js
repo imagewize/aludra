ff76b4eb02fe49913d3d183784c35c798cda31ef
Jasper Frumau <jasper@imagewize.com>
2026-07-11 09:25:11 +0700

    Add Testimonial Grid block
    Grid of customer testimonials with metrics that automatically
    becomes a Slick Carousel on larger sets (4+ cards on desktop,
    2+ on mobile), otherwise renders as a static grid.

---
+++ blocks/testimonial-grid/src/index.js
@@ -0,0 +1,21 @@
+/**
+ * WordPress dependencies
+ */
+import { registerBlockType } from '@wordpress/blocks';
+
+/**
+ * Internal dependencies
+ */
+import metadata from './block.json';
+import Edit from './edit';
+import Save from './save';
+import './style.scss';
+
+/**
+ * Register the block
+ */
+registerBlockType( metadata.name, {
+	...metadata,
+	edit: Edit,
+	save: Save,
+} );
