dff441bb2762a4cefd6395fb967f502b3d9d9625
Jasper Frumau <jasper@imagewize.com>
2026-07-21 10:47:51 +0700

    Bump carousel block version to 1.1.0 for rail mode feature
    Version bump to reflect the new engine attribute and rail mode rendering.

--- blocks/carousel/build/index.asset.php
+++ blocks/carousel/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '65655d230f9ce019fca7');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3802bf080a419a59b7bd');
